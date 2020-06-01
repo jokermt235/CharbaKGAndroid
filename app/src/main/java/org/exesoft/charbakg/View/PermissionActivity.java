d56f1e58467289a48e6dfc773fe99b472ec7ed60
Mateo <joker1988_88@mail.com>
2020-06-01 09:05:05 +0600

    Add user farm permission request

---
+++ app/src/main/java/org/exesoft/charbakg/View/PermissionActivity.java
@@ -0,0 +1,16 @@
+package org.exesoft.charbakg.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.exesoft.charbakg.R;
+
+public class PermissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+    }
+}
--- app/src/main/java/org/exesoft/charbakg/View/SplashActivity.java
+++ app/src/main/java/org/exesoft/charbakg/View/SplashActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import org.exesoft.charbakg.Callback.OnSimpleLoaderResult;
 import org.exesoft.charbakg.Component.Auth;
+import org.exesoft.charbakg.Controller.SimpleLoader;
 import org.exesoft.charbakg.R;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -17,8 +22,18 @@ public class SplashActivity extends AppCompatActivity {
         if(!Auth.isSigned()){
             startActivity(new Intent(this, LoginActivity.class));
         }else{
+            SimpleLoader.filter("user",new OnSimpleLoaderResult(){
+                @Override
+                public void onResult(ArrayList<Map<String, Object>> items) {
+                    super.onResult(items);
+                    if(!items.isEmpty()){
+                        if((boolean)items.get(0).get("status")){
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        }
+                    }
 
-            startActivity(new Intent(this, HomeActivity.class));
+                }
+            });
         }
         finish();
     }
